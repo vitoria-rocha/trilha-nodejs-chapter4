0f82726ba8ccc38406eecade084c325b3ccf361f
vitoria-rocha <vitoria.rocha1206@gmail.com>
2021-11-22 14:37:33 -0400

    --ESTRUTURANDO A ENTIDADE CARROS

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,30 +1,50 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+import { Category } from "./Category";
+
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id: string;
  
+  @Column()
   name: string;
 
+  @Column()
   description: string;
  
+  @Column()
   daily_rate: number;
 
+  @Column()
   available: boolean;
+  //available: boolean;
 
+  @Column()
   license_plate: string;
 
+  @Column()
   fine_amount: number;
 
+  @Column()
   brand: string;
- 
+
+ // muitos carros para 1 categoria, many to one
+  @ManyToOne(() => Category)
+  @JoinColumn({name: "category_id"})
+  category: Category;
+
+  @Column()
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
   constructor(){
     if(!this.id){
       this.id = uuidv4();
       this.available = true;
-      this.created_at = new Date();
+      //this.created_at = new Date();
     }
   }
 }
--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -13,7 +13,7 @@ describe("Create car", ()=>{
   });
 
   it("should be able to create a new car", async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: "Name Car",
       description: "Description Car",
       daily_rate: 100,
@@ -22,6 +22,8 @@ describe("Create car", ()=>{
       brand: "Brand",
       category_id: "category",
     });
+
+    expect(car).toHaveProperty("id");
   });
 
   it("should be able to create a car with exists license plate", () => {
